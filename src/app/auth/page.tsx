b83c655fc7107db4dc1df442b59eb2e5b2786c6d
haewonwon <khcw1029@daum.net>
2025-08-08 03:08:59 +0900

    Feat: Login 페이지 구현

--- src/app/auth/page.tsx
+++ src/app/auth/page.tsx
@@ -1,15 +1,25 @@
 'use client';
 
 import React from 'react';
-import LoginForm from '@/components/auth/LoginForm';
-import { Container, Flex, Button } from '@chakra-ui/react';
+import { Container, Flex, Button, Heading, Box, Text } from '@chakra-ui/react';
+import { LoginForm, AutoLoginCheckbox } from '@/components/auth';
 // import { Toaster, toaster } from '@/components/ui/Toaster';
 
 export default function AuthPage() {
   return (
-    <Container maxW="1920px" py={8}>
-      <Flex justify="center" align="center" minH="100vh">
-        <LoginForm />
+    <Container maxW="1920px">
+      <Flex align="center" justify="center" minH="100vh">
+        <Box w="100%" maxW="27rem">
+          <Heading size="3xl" mb={6} textAlign="left" color="blue.500">
+            로그인
+          </Heading>
+          <LoginForm />
+          <AutoLoginCheckbox />
+          <Box borderTop="1px solid" borderColor="blue.500" my={4} />
+          <Text mt="4" color="gray.500" fontSize="xs" textAlign="left">
+            별도 회원가입 절차 없이, 로그인 한 번으로 바로 가입이 완료됩니다.
+          </Text>
+        </Box>
       </Flex>
     </Container>
   );

87931892bf62ac41b3fa17441ed6da55906ef1ef
haewonwon <khcw1029@daum.net>
2025-08-18 07:17:30 +0900

    Fix: Select UI 추가됨에 따라 index 수정

--- src/components/ui/index.ts
+++ src/components/ui/index.ts
@@ -3,3 +3,4 @@ export { default as Button } from './Button';
 export { default as Modal } from './Modal';
 export { default as Tooltip } from './Tooltip';
 export { default as Pagination } from './Pagination';
+export { default as Select } from './Select';

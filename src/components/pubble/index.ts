cad09f5e35215abaae69834b1ead3d4a7c463c5f
haewonwon <khcw1029@daum.net>
2025-08-18 07:24:50 +0900

    Fix: 퍼블 컴포넌트 추가됨에 따라 index 파일 수정

--- src/components/pubble/index.ts
+++ src/components/pubble/index.ts
@@ -1,3 +1,8 @@
 export { default as Editor } from './Editor';
 export { default as ToolBar } from './ToolBar';
 export { default as PubbleHeader } from './PubbleHeader';
+export { default as PostAuthor } from './PostAuthor';
+export { default as PostBody } from './PostBody';
+export { default as CommentsSection } from './CommentsSection';
+export { default as CommentsModal } from './CommentsModal';
+export { default as PostNavigation } from './AuthorPosts';

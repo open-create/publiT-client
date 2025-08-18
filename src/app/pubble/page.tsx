1ab7be93c587255a88ee346fcf7bf52f13497a43
haewonwon <khcw1029@daum.net>
2025-08-18 19:30:57 +0900

    Fix: tailwind 기반 코드 삭제

--- src/app/pubble/page.tsx
+++ src/app/pubble/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { VStack } from '@chakra-ui/react';
 import Editor from '@/components/pubble/Editor';
 
 export default function PublitPage() {
   return (
-    <div className="space-y-6">
+    <VStack align="stretch" gap={6}>
       <Editor />
-    </div>
+    </VStack>
   );
 }
